cad7e7274e38f13de473b1ecb90266d6c0ca039e
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-06-17 11:38:30 +0530

    Add files via upload

---
+++ OxyXmusic/config.py
@@ -0,0 +1,29 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "OxyXupdates")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/a765ac304f4d6f507e544.jpg")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Oxy_VC_01")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "OxyXSupport")
+PROJECT_NAME = getenv("PROJECT_NAME", "OxyXmusic")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/OxyNotOp/OxyVCbot")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
