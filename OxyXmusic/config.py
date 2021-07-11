bb0620c04f24c4e66aaf678bb890c5ac1a049795
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-07-11 19:51:47 +0530

    Add files via upload

--- OxyXmusic/config.py
+++ OxyXmusic/config.py
@@ -1,6 +1,23 @@
-import os
-from os import getenv
+# OxyXmusic- Telegram bot project
+# Copyright (C) 2021  Roj Serbest
+# Copyright (C) 2021  Inuka Asith
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modified by Inukaasith
+
+import os
+from os import path
+from os import getenv
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
@@ -10,20 +27,19 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "OxyXupdates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/a765ac304f4d6f507e544.jpg")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
 admins = {}
-API_ID = int(getenv("API_ID"))
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Oxy_VC_01")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "OxyXSupport")
-PROJECT_NAME = getenv("PROJECT_NAME", "OxyXmusic")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/OxyNotOp/OxyVCbot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "DaisyXhelper")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DaisySupport_Official")
+PROJECT_NAME = getenv("PROJECT_NAME", "OxyXmusic v5")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/TeamDaisyX/OxyXmusic")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-PLAYLIST_PIC = getenv("PLAYLIST_PIC", "https://telegra.ph/file/b11ffe2a2edb0869adb44.jpg")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
